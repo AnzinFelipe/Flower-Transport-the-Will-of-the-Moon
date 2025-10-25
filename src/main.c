6d7a5d017b1fdc75db44366937db0dbbf801b1b7
AnzinFelipe <ian.felipe.drunen@gmail.com>
2025-10-25 17:55:07 +0000

    Ajeitando alguns erros pra rodar as imagens

--- src/main.c
+++ src/main.c
@@ -146,10 +146,10 @@ int main(void) {
         DrawRectangle(1215, 615, 370, 35, vida_cor);
         DrawRectangle(1550, 650, 35, 250, BLACK);
         if (horario == 1) {
-            DrawTextureEX(noite, (Vector2){0, 0}, 0.0, 1, WHITE)
+            DrawTextureEx(noite, (Vector2){0, 0}, 0.0, 1, WHITE);
             DrawTextureEx(quadro_roxo_noite, (Vector2){15, 650}, 0.0, 0.35, WHITE);
             DrawTextureEx(quadro_vermelho_noite, (Vector2){415, 650}, 0.0, 0.35, WHITE);
-        } else (horario == 0) {
+        } else if (horario == 0) {
             DrawTextureEx(quadro_roxo, (Vector2){15, 650}, 0.0, 0.35, WHITE);
             DrawTextureEx(quadro_vermelho, (Vector2){415, 650}, 0.0, 0.35, WHITE);
         }
