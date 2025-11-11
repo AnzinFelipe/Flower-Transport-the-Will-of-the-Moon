d18129ece9f43f6b8875fa567cf3c087109150a3
Rafael Chiappetta Barboza <rafaelchiappettabarboza@gmail.com>
2025-11-11 00:19:58 +0000

    Inicializa todos os ponteiros da struct na função de inicializar variaveis.

--- src/inicia_libera_jogo.c
+++ src/inicia_libera_jogo.c
@@ -2,5 +2,12 @@
 #include <stdlib.h>
 #include <string.h>
 
-void novo_jogo(VariaveisInicioJogo *s, int largura, int altura){}
+void novo_jogo(VariaveisInicioJogo *s, int largura, int altura){
+    s->ataque_head2 = NULL; s->ataque_head3 = NULL; s->ataque_head4 = NULL; s->ataque_head_boss = NULL;
+    s->flor_dia_head1 = NULL; s->flor_dia_head2 = NULL;
+    s->flor_noite_head1 = NULL; s->flor_noite_head3 = NULL;
+    s->personagem_atual = NULL; s->personagem_head = NULL;
+    s->boss = NULL;
+    s->elemento_atual = NULL;
+}
 void liberar_jogo(VariaveisInicioJogo *s){}
